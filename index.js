c2d42284fa2f90b5a3c1ed56bc771b16493cdb98
rzatalibov555 <sethubpro@gmail.com>
2023-08-27 17:39:20 +0400

    sebuhi task

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function click_et(){
+
+    // document.querySelector(".center").style.right= "0px";
+    // document.querySelector(".center").style.left= "0px";
+    document.querySelector(".leftside").classList.toggle("leftx")
+    document.querySelector(".rightside").classList.toggle("rightx")
+
+
+}
